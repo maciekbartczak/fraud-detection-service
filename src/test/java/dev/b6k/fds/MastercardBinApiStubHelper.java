b56d81a35c7ca15ed6737f45534954092c57804a
Maciej Bartczak <39600846+maciekbartczak@users.noreply.github.com>
2025-03-23 17:39:06 +0100

    Implement tests for risk assessment api

--- src/test/java/dev/b6k/fds/MastercardBinApiStubHelper.java
+++ src/test/java/dev/b6k/fds/MastercardBinApiStubHelper.java
@@ -1,51 +1,55 @@
 package dev.b6k.fds;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.b6k.fds.integration.mastercard.bin.model.BinResource;
+import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
+
+import java.util.function.Consumer;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @UtilityClass
 public class MastercardBinApiStubHelper {
+    private static final String BIN_RESOURCE_JSON = """
+            {
+              "lowAccountRange": 5221696500000000000,
+              "highAccountRange": 5221696600000000000,
+              "binNum": "123456",
+              "binLength": 6,
+              "acceptanceBrand": "MCC",
+              "ica": "00000022053",
+              "customerName": "DFCC BANK PLC",
+              "country": {
+                "code": 144,
+                "alpha3": "LKA",
+                "name": "Sri Lanka"
+              },
+              "localUse": false,
+              "authorizationOnly": false,
+              "productCode": "MCW",
+              "productDescription": "WORLD",
+              "governmentRange": false,
+              "nonReloadableIndicator": false,
+              "anonymousPrepaidIndicator": "N",
+              "cardholderCurrencyIndicator": "D",
+              "billingCurrencyDefault": "LKR",
+              "programName": null,
+              "vertical": null,
+              "fundingSource": "CREDIT",
+              "consumerType": "CONSUMER",
+              "smartDataEnabled": true,
+              "affiliate": null,
+              "comboCardIndicator": "N",
+              "flexCardIndicator": "N",
+              "gamblingBlockEnabled": false,
+              "paymentAccountType": "P"
+            }
+            """;
     private static final String EMPTY_BIN_RANGE_RESPONSE_BODY = "[]";
-    private static final String BIN_RANGE_RESPONSE_BODY = """
-            [
-              {
-                "lowAccountRange": 5221696500000000000,
-                "highAccountRange": 5221696600000000000,
-                "binNum": "123456",
-                "binLength": 6,
-                "acceptanceBrand": "MCC",
-                "ica": "00000022053",
-                "customerName": "DFCC BANK PLC",
-                "country": {
-                  "code": 144,
-                  "alpha3": "LKA",
-                  "name": "Sri Lanka"
-                },
-                "localUse": false,
-                "authorizationOnly": false,
-                "productCode": "MCW",
-                "productDescription": "WORLD",
-                "governmentRange": false,
-                "nonReloadableIndicator": false,
-                "anonymousPrepaidIndicator": "N",
-                "cardholderCurrencyIndicator": "D",
-                "billingCurrencyDefault": "LKR",
-                "programName": null,
-                "vertical": null,
-                "fundingSource": "CREDIT",
-                "consumerType": "CONSUMER",
-                "smartDataEnabled": true,
-                "affiliate": null,
-                "comboCardIndicator": "N",
-                "flexCardIndicator": "N",
-                "gamblingBlockEnabled": false,
-                "paymentAccountType": "P"
-              }
-            ]""";
 
     public static void prepareSuccessResponse(String bin) {
-        prepareServiceResponse(bin, BIN_RANGE_RESPONSE_BODY);
+        prepareServiceResponse(bin, makeSingleBinLookupResponseBody(BIN_RESOURCE_JSON));
     }
 
     public static void prepareNoDataResponse(String bin) {
@@ -62,4 +66,25 @@ public class MastercardBinApiStubHelper {
                                 .withBody(responseBody))
                 );
     }
+
+    @SneakyThrows
+    public static void prepareCustomServiceResponse(String bin, Consumer<BinResource> apiResponseCustomizer) {
+        var objectMapper = new ObjectMapper();
+        var binResource = objectMapper.readValue(BIN_RESOURCE_JSON, BinResource.class);
+        apiResponseCustomizer.accept(binResource);
+
+        WireMockExtension.getWireMockServer()
+                .stubFor(post(urlEqualTo("/bin-ranges/account-searches"))
+                        .withRequestBody(matchingJsonPath("$.accountRange", equalTo(bin)))
+                        .willReturn(aResponse()
+                                .withStatus(200)
+                                .withHeader("Content-Type", "application/json")
+                                .withBody(makeSingleBinLookupResponseBody(objectMapper.writeValueAsString(binResource))))
+                );
+    }
+
+    private String makeSingleBinLookupResponseBody(String rawBinResourceJson) {
+        return String.format("[%s]", rawBinResourceJson);
+    }
+
 }
--- src/test/java/dev/b6k/fds/riskassessment/TransactionTransactionRiskAssessmentHttpEndpointTest.java
+++ src/test/java/dev/b6k/fds/riskassessment/TransactionTransactionRiskAssessmentHttpEndpointTest.java
@@ -3,33 +3,48 @@ package dev.b6k.fds.riskassessment;
 import dev.b6k.fds.MastercardBinApiStubHelper;
 import dev.b6k.fds.MastercardBinApiTestProfile;
 import dev.b6k.fds.WireMockExtension;
+import dev.b6k.fds.integration.mastercard.bin.model.BinResourceCountry;
 import dev.b6k.fds.model.TransactionRiskAssessmentRequest;
 import dev.b6k.fds.model.TransactionRiskAssessmentRequestLocation;
 import dev.b6k.fds.model.TransactionRiskAssessmentResponse;
+import dev.b6k.fds.model.TransactionRiskAssessmentResponseRiskFactorsInner;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 @QuarkusTestResource(WireMockExtension.class)
 @TestProfile(MastercardBinApiTestProfile.class)
+// TODO: load custom weights, thresholds and scores for assertions
 public class TransactionTransactionRiskAssessmentHttpEndpointTest {
+    @BeforeEach
+    void setup() {
+        WireMockExtension.getWireMockServer().resetAll();
+    }
 
     @Test
     void assessRiskForLowRiskTransaction() {
         // given
         var binNumber = "123456";
-        MastercardBinApiStubHelper.prepareSuccessResponse(binNumber);
+        MastercardBinApiStubHelper.prepareCustomServiceResponse(binNumber, response -> {
+            var country = new BinResourceCountry();
+            country.setAlpha3("POL");
+            country.setName("Poland");
+
+            response.setCountry(country);
+        });
 
         var request = TransactionRiskAssessmentRequest.builder()
                 .bin(new BigDecimal(binNumber))
-                .amount(new BigDecimal("2000.00"))
+                .amount(new BigDecimal("13.74"))
                 .currency("PLN")
                 .location(TransactionRiskAssessmentRequestLocation.builder()
                         .countryCode("POL")
@@ -41,7 +56,49 @@ public class TransactionTransactionRiskAssessmentHttpEndpointTest {
 
         // then
         assertEquals(TransactionRiskAssessmentResponse.RiskLevelEnum.LOW, response.getRiskLevel());
+        assertEquals(13, response.getRiskScore());
+        assertThat(response.getRiskFactors())
+                .hasSize(1)
+                .first()
+                .satisfies(riskFactor -> {
+                    assertEquals("FIRST_TRANSACTION", riskFactor.getFactorName());
+                    assertEquals("First transaction with this card", riskFactor.getFactorDescription());
+                });
     }
+
+    @Test
+    void assessRiskForMediumRiskTransaction() {
+        // given
+        var binNumber = "674523";
+        MastercardBinApiStubHelper.prepareCustomServiceResponse(binNumber, response -> {
+            var country = new BinResourceCountry();
+            country.setAlpha3("POL");
+            country.setName("Poland");
+
+            response.setCountry(country);
+        });
+
+        var request = TransactionRiskAssessmentRequest.builder()
+                .bin(new BigDecimal(binNumber))
+                .amount(new BigDecimal("100.00"))
+                .currency("PLN")
+                .location(TransactionRiskAssessmentRequestLocation.builder()
+                        .countryCode("ARG")
+                        .build())
+                .build();
+
+        // when
+        var response = callRiskAssessmentService(request, 200, TransactionRiskAssessmentResponse.class);
+
+        // then
+        assertEquals(TransactionRiskAssessmentResponse.RiskLevelEnum.MEDIUM, response.getRiskLevel());
+        assertEquals(40, response.getRiskScore());
+        assertThat(response.getRiskFactors())
+                .hasSize(3)
+                .extracting(TransactionRiskAssessmentResponseRiskFactorsInner::getFactorName)
+                .containsExactlyInAnyOrder("FOREIGN_CARD", "ROUND_TRANSACTION_AMOUNT", "FIRST_TRANSACTION");
+    }
+
 
     private <T> T callRiskAssessmentService(TransactionRiskAssessmentRequest request, int expectedStatusCode, Class<T> responseClass) {
         return given()
